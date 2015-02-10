d649d1f93621e70d73621806065cf5a72284fb15
Bearuser <bearuser@codelion.net>
2015-02-10 06:17:16 +0000

    mvc2 이메일 저장 페이지 만들기

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,5 +6,9 @@ class HomeController < ApplicationController
   end
 
   def add_process
+		a = Address.new
+		n.name = params[:email_user]
+		n.email_address = params[:email_address]
+		n.save
   end
 end
